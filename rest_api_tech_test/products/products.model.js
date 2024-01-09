5cc68df3acf1f7f67f5e7f494483fae4ae3e00bb
David Mason <david_mason_@hotmail.co.uk>
2024-01-09 14:58:30 +0000

    Product model function for getting product by stock number complete

---
+++ rest_api_tech_test/products/products.model.js
@@ -0,0 +1,20 @@
+// // Imports
+import fs from "node:fs/promises";
+
+// Constant for the file path
+const filePath = "./rest_api_tech_test/database/data.json";
+
+// Function to get a product by stock number from the JSON data file
+const getByID = async (stockNumber) => {
+    // Fetch the data file array as a JSON
+    const productsJSON = await fs.readFile(filePath);
+
+    // Parse the JSON data array
+    const products = JSON.parse(productsJSON);
+
+    // Filter the array to find matching product
+    const matchedProduct = products.filter(obj => obj.stock_number === stockNumber);
+
+    // Return either the product object or null
+    return matchedProduct.length > 0 ? matchedProduct[0] : null;
+};
