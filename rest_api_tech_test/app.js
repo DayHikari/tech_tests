9b38d4c92e5e8d7f3981577394ade59f4de37f6d
David Mason <david_mason_@hotmail.co.uk>
2024-01-09 14:34:20 +0000

    Server file complete and App file updated to include basics

---
+++ rest_api_tech_test/app.js
@@ -0,0 +1,10 @@
+// // Imports
+import express from "express";
+
+// App constant for express
+const app = express();
+
+app.use(express.json);
+
+
+export default app;
---
+++ rest_api_tech_test/server.js
@@ -0,0 +1,10 @@
+// // Imports
+import app from "./app.js";
+
+// Port constant using test requirement
+const port = 8080;
+
+// Set the server listening for the port
+app.listen(port, () => {
+    console.log(`Server is listening to port ${port}`);
+})
