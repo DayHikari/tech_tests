0d6bf8ad00575b9c857ab081b868f87d0fae0d56
David Mason <david_mason_@hotmail.co.uk>
2024-01-25 20:31:17 +0000

    MongoDB request updated to remove need for object fixer function

--- rest_api_tech_test/products/productsModel.js
+++ rest_api_tech_test/products/productsModel.js
@@ -15,39 +15,6 @@ export const connectToDb = async (database) => {
   db = connection.db(database);
 };
 
-// Function to remove MongoDB id when returning object
-const objectFixer = (products) => {
-  // Declare the empty variable
-  let productList;
-
-  // Check if the products is an array
-  if (Array.isArray(products)) {
-    // Map through products to remove the id number
-    productList = products.map((obj) => {
-      // Create the new obj with no id
-      const newObj = {
-        stock_number: obj.stock_number,
-        name: obj.name,
-        Description: obj.Description,
-        Price: obj.Price,
-      };
-
-      // Return the new obj to set to variable
-      return newObj;
-    });
-  } else {
-    productList = {
-      stock_number: products.stock_number,
-      name: products.name,
-      Description: products.Description,
-      Price: products.Price,
-    };
-  }
-
-  // Return product list
-  return productList;
-};
-
 // Function to get all products
 export const getProducts = async () => {
   // Declare variable to store product information
@@ -56,41 +23,21 @@ export const getProducts = async () => {
   // Send Get request to Mongo database to find all products and push each to products array
   await db
     .collection("product_db")
-    .find()
+    .find({}, { projection: {_id: 0} })
     .forEach((product) => products.push(product));
 
-  // Error handling
-  if (products.length === 0) {
-    return null;
-  }
-
-  // Map through products to remove the id number
-  const productList = objectFixer(products);
-
   // Return the product list
-  return productList;
+  return products.length !== 0 ? products : null;
 };
 
 // Function to get a product by stock number
 export const getByStockNumber = async (stockNumber) => {
-  // Declare variable to store product info
-  let mongoProduct;
-
   // Send get request while finding the stock number
-  await db
+  const product = await db
     .collection("product_db")
-    .findOne({ stock_number: stockNumber })
-    .then((product) => {
-      mongoProduct = product;
-    })
-    .catch((err) => {
-      mongoProduct = null;
-    });
+    .findOne({ stock_number: stockNumber }, { projection: {_id: 0} });
 
-  // Correct the object format
-  const product = mongoProduct ? objectFixer(mongoProduct) : null;
-
-  // Return the corrected product
+  // Return the product or null
   return product;
 };
 
@@ -101,7 +48,7 @@ export const addNewProduct = async (newProduct) => {
   //Send the post request
   await db
     .collection("product_db")
-    .insertOne(newProduct, {writeConcern: {forceServerObjectId: true}})
+    .insertOne(newProduct)
     .then((result) => {
       confirmation = result.acknowledged;
     });
